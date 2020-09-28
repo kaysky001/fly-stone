d7f1e6f247d710b2c67ec2699c45f65fe258598c
kaysky001 <772568839@qq.com>
2020-09-28 10:32:06 +0900

    DB

--- flystone-rest-api/src/config/db.config.js
+++ flystone-rest-api/src/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: 'localhost',
-  USER: 'hailang',
-  PASSWORD: '110119',
-  DB: 'flystone',
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "kaku123456",
+  DB: "flystone",
 };
